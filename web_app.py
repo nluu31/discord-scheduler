00f377ee3c6c40432b600cb740283ad346b6c245
nluu <mrnick731@gmail.com>
2025-09-01 15:35:33 -0700

    fixed web_app.py port

--- web_app.py
+++ web_app.py
@@ -540,4 +540,5 @@ def format_date(date_str):
 if __name__ == '__main__':
     with app.app_context():
         init_db()
-    app.run(debug=True)
+    port = int(os.getenv('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
